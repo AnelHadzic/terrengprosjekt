596dbbb96ee9dc4a5ed88401f765750de233e7f3
Marius A <mariusca@hiof.no>
2023-09-21 20:45:38 +0200

    Added a non functional login page. Design testing.

--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -1,7 +1,59 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
+                <form className="space-y-6" action="#">
+                    <h5 className="text-xl font-medium text-gray-900 dark:text-white">
+                        Værste login
+                    </h5>
+                    <div>
+                        <label
+                            htmlFor="email"
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                        >
+                            Din jobb e-post
+                        </label>
+                        <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                            placeholder="navn@bedrift.no"
+                            required
+                        />
+                    </div>
+
+                    <div className="flex items-start">
+                        <div className="flex items-start">
+                            <div className="flex items-center h-5">
+                                <input
+                                    id="remember"
+                                    type="checkbox"
+                                    value=""
+                                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                                    required
+                                />
+                            </div>
+                            <label
+                                htmlFor="remember"
+                                className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+                            >
+                                Remember me
+                            </label>
+                        </div>
+                    </div>
+                    <button
+                        type="submit"
+                        className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                        Logg inn
+                    </button>
+                </form>
+            </div>
+        </main>
+    );
 };
 
 export default page;
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { useRouter } from "next/navigation";
 export default function Home() {
     const router = useRouter();
 
-    const handleClick = () => {
-        router.push("/db-test");
+    const handleClick = (route: string) => {
+        router.push(route);
     };
 
     return (
@@ -13,9 +13,14 @@ export default function Home() {
             <button
                 type="button"
                 className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
-                onClick={handleClick}
-            >
+                onClick={() => handleClick("/db-test")}>
                 Go to database test
+            </button>
+            <button
+                type="button"
+                className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+                onClick={() => handleClick("/login")}>
+                Go to login
             </button>
         </main>
     );
