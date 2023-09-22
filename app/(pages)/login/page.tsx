ac82b390dd823765cb8bbaa26ea38a7bd4c5eae4
Marius A <mariusca@hiof.no>
2023-09-22 07:51:42 +0200

    Satte opp mulig login design

--- app/(pages)/login/page.tsx
+++ app/(pages)/login/page.tsx
@@ -24,26 +24,6 @@ const page = () => {
                             required
                         />
                     </div>
-
-                    <div className="flex items-start">
-                        <div className="flex items-start">
-                            <div className="flex items-center h-5">
-                                <input
-                                    id="remember"
-                                    type="checkbox"
-                                    value=""
-                                    className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
-                                    required
-                                />
-                            </div>
-                            <label
-                                htmlFor="remember"
-                                className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                            >
-                                Remember me
-                            </label>
-                        </div>
-                    </div>
                     <button
                         type="submit"
                         className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
