2fbad3c7782ad994d166d7da43fc9805112e72a0
Anel Hadzic <77800103+AnelHadzic@users.noreply.github.com>
2023-09-29 12:33:23 +0200

    Added parkinglot interface and model

---
+++ app/lib/model/parkinglot.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+import { IParkingLot } from '../interface/IParkingLot';
+
+const parkingLotSchema = new Schema({
+    parkingName: {
+        type: String,
+        unique: true,
+    },
+    parkingCapacity: String,
+    created: { type: Date, default: Date.now },
+});
+
+export const ParkingLot = mongoose.model<IParkingLot>("ParkingLot", parkingLotSchema);
+
+export async function findParkingLot(parkingName: string | null) {
+    const session = await ParkingLot.findOne({ parkingName: { $eq: parkingName } });
+
+    if (session) {
+        return session;
+    }
+
+    return null;
+}
+
+export async function createParkingLot(parkingLot: IParkingLot) {
+    const newEntry = new ParkingLot(parkingLot);
+    await newEntry.save();
+}
