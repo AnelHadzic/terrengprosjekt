6a175fbd3285877b24eea83d2fc9c80fb3b0012d
Marius A <mariusca@hiof.no>
2023-10-01 10:24:30 +0200

    Shortened company endpoint. Fix bug in GET single.

--- app/api/company/[id]/route.ts
+++ app/api/company/[id]/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 import mongoose from "mongoose";
 import { findCompany } from "@/app/lib/model/company";
 
-export async function GET(context: { params: any }) {
+export async function GET(request: Request, context: { params: any }) {
     await connectToDb();
     const companyId = context.params.id;
 
--- app/api/company/route.ts
+++ app/api/company/route.ts
@@ -2,31 +2,20 @@ import connectToDb from "@/app/lib/db/mongoose";
 import {
     findAllCompanies,
     findCompaniesByName,
-    findCompany,
 } from "@/app/lib/model/company";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest, context: { params: any }) {
+export async function GET({ nextUrl }: NextRequest) {
     await connectToDb();
-    const searchParams = request.nextUrl.searchParams;
-    const companyName = searchParams.get("companyName");
-    if (companyName != null) {
-        const companies = await findCompaniesByName(companyName);
-        if (companies === null) {
-            return NextResponse.json(
-                { data: "Database operation error" },
-                { status: 500 }
-            );
-        }
-        return NextResponse.json({ data: companies }, { status: 200 });
-    } else {
-        const companies = await findAllCompanies();
-        if (companies === null) {
-            return NextResponse.json(
-                { data: "Database operation error" },
-                { status: 500 }
-            );
-        }
-        return NextResponse.json({ data: companies }, { status: 200 });
+    const companyName = nextUrl?.searchParams?.get("companyName");
+    const companies = companyName
+      ? await findCompaniesByName(companyName)
+      : await findAllCompanies();
+  
+    if (companies === null) {
+      return NextResponse.json({ data: "Database operation error" }, { status: 500 });
     }
-}
+  
+    return NextResponse.json({ data: companies }, { status: 200 });
+  }
+  
