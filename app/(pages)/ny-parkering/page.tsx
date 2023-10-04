7a85bce0612bbe8f2b06f7bebaf92f6769ef3a34
Anel Hadzic <77800103+AnelHadzic@users.noreply.github.com>
2023-10-04 02:50:54 +0200

    Added some contents and API's for parkingLots

---
+++ app/(pages)/ny-parkering/page.tsx
@@ -0,0 +1,52 @@
+import ParkeringsListe from "@/app/components/parkering/ParkeringsListe";
+import React from "react";
+
+const Page = () => {
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center">
+        <form>
+          <div className="mb-6">
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Parkeringsnavn
+            </label>
+            <input
+              type="text"
+              id="text"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="P1"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Kapasitet
+            </label>
+            <input
+              type="number"
+              id="number"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Add
+          </button>
+        </form>
+
+        <ParkeringsListe />
+      </main>
+    </>
+  );
+};
+
+export default Page;
