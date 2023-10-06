8a0c73a8bb4ac4d5d3651f36e3f025b16e4dacfd
Marius Christian Aastebøl <mariusca@hiof.no>
2023-10-06 13:28:05 +0200

    Set up overview page for each company dynamicly

--- app/(pages)/bedrifter/[id]/page.tsx
+++ app/(pages)/bedrifter/[id]/page.tsx
@@ -6,13 +6,13 @@ export default function Page({ params }: { params: { id: string } }) {
   const router = useRouter();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SingleCompany />
+      <SingleCompany companyId={params.id} />
       <button
         type="button"
         onClick={() => router.push("/bedrifter")}
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
       >
-        return
+        Gå tilbake til liste
       </button>
     </main>
   );
--- app/components/Bedrifter/SingleCompany.tsx
+++ app/components/Bedrifter/SingleCompany.tsx
@@ -1,99 +1,159 @@
-import Image from "next/image";
+import axios from "axios";
+import { ICompany } from "@/app/lib/interface/ICompany";
+import { Fragment, useEffect, useState } from "react";
 
-export default function SingleCompany() {
-    return (
-        <div className="w-full p-6 mt-6 pb-12 flex">
-            {/* Details */}
-            <div className="w-1/3 pl-14 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <h1 className="text-xl mb-6 font-bold leading-none text-gray-900 dark:text-white">
-                    Sopra Steria
-                </h1>
-                <h2 className="text-l font-bold leading-none text-gray-900 dark:text-white">
-                    Kontaktperson
-                </h2>
-                <p className="mb-6">bossman@sopra.no</p>
+export default function SingleCompany({ companyId }: { companyId: string }) {
+  const [company, setCompany] = useState<ICompany>();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const API_URL = `http://localhost:3000/api/company/${companyId}`;
+        const response = await axios.get(API_URL);
+        console.log(response.data.data);
 
-                <h2 className="text-l font-bold leading-none text-gray-900 dark:text-white">
-                    Bedriftsavtale
-                </h2>
-                <p className="mb-6">E-poster: person@sopra.no</p>
+        setCompany(response.data.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-                <h2 className="text-l font-bold leading-none text-gray-900 dark:text-white">
-                    Privat avtale
-                </h2>
-                <p className="mb-6">Domener: sopra.no</p>
+    fetchData();
+  }, [companyId]);
 
-                <div className="flex items-center justify-between">
-                    <h2 className="text-l font-bold leading-none text-gray-900 dark:text-white">
-                        Antall brukere
-                    </h2>
-                    <a
-                        href="#"
-                        className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
-                    >
-                        Se alle
-                    </a>
-                </div>
-                <p className="mb-6">85</p>
-            </div>
+  return (
+    <div className="w-full p-6 mt-6 pb-12 flex">
+      {/* Details */}
+      <div className="w-1/3 pl-14 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <h1 className="text-xl mb-6 font-bold leading-none text-gray-900 dark:text-white">
+          {company?.companyName}
+        </h1>
+        <h2 className="text-l font-bold leading-none text-gray-900 dark:text-white">
+          Kontaktperson
+        </h2>
+        <p className="mb-6">{company?.contactEmail}</p>
 
-            {/* History */}
-            <div className="w-2/3 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-                    <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                        <tr>
-                            <th scope="col" className="px-6 py-3">
-                                Dato
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                Hendelse
-                            </th>
-                            <th scope="col" className="px-6 py-3">
-                                Utført av
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <th
-                                scope="row"
-                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                            >
-                                06.10.2023
-                            </th>
-                            <td className="px-6 py-4">
-                                Endret kontaktperson.
-                            </td>
-                            <td className="px-6 py-4">mariusca@hiof.no</td>
-                        </tr>
-                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <th
-                                scope="row"
-                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                            >
-                                06.10.2023
-                            </th>
-                            <td className="px-6 py-4">
-                                Lang tekst for å forklare nøyaktig hva som
-                                skjedde med denne bedriften.
-                            </td>
-                            <td className="px-6 py-4">mariusca@hiof.no</td>
-                        </tr>
-                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <th
-                                scope="row"
-                                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                            >
-                                06.10.2023
-                            </th>
-                            <td className="px-6 py-4">
-                                Bedriften ble opprettet
-                            </td>
-                            <td className="px-6 py-4">mariusca@hiof.no</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+        {company?.companyAgreement ? (
+          <>
+            <h2 className="text-l font-bold leading-none text-gray-900 dark:text-white">
+              Bedriftsavtale
+            </h2>
+            {company.companyAgreement.domains ? (
+              <div className="mb-6">
+                <p>Domener:</p>
+                {company.companyAgreement.domains.map((item, index) => (
+                  <Fragment key={index}>
+                    <p>{item}</p>
+                  </Fragment>
+                ))}
+              </div>
+            ) : company.companyAgreement.emails ? (
+              <div className="mb-6">
+                <p>e-poster:</p>
+                {company.companyAgreement.emails.map((item, index) => (
+                  <Fragment key={index}>
+                    <p>{item}</p>
+                  </Fragment>
+                ))}
+              </div>
+            ) : null}
+            <div className="mb-6"></div>
+          </>
+        ) : null}
+
+        {company?.privateAgreement ? (
+          <>
+            <h2 className="text-l font-bold leading-none text-gray-900 dark:text-white">
+              Privat avtale
+            </h2>
+            {company.privateAgreement.domains ? (
+              <div className="mb-6">
+                <p>Domener:</p>
+                {company.privateAgreement.domains.map((item, index) => (
+                  <Fragment key={index}>
+                    <p>{item}</p>
+                  </Fragment>
+                ))}
+              </div>
+            ) : company.privateAgreement.emails ? (
+              <div className="mb-6">
+                <p>e-poster:</p>
+                {company.privateAgreement.emails.map((item, index) => (
+                  <Fragment key={index}>
+                    <p>{item}</p>
+                  </Fragment>
+                ))}
+              </div>
+            ) : null}
+          </>
+        ) : null}
+
+        <div className="flex items-center justify-between">
+          <h2 className="text-l font-bold leading-none text-gray-900 dark:text-white">
+            Antall brukere
+          </h2>
+          <a
+            href="#"
+            className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
+          >
+            Se alle
+          </a>
         </div>
-    );
+        <p className="mb-6">85</p>
+      </div>
+
+      {/* History */}
+      <div className="w-2/3 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Dato
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Hendelse
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Utført av
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                06.10.2023
+              </th>
+              <td className="px-6 py-4">Endret kontaktperson.</td>
+              <td className="px-6 py-4">mariusca@hiof.no</td>
+            </tr>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                06.10.2023
+              </th>
+              <td className="px-6 py-4">
+                Lang tekst for å forklare nøyaktig hva som skjedde med denne
+                bedriften.
+              </td>
+              <td className="px-6 py-4">mariusca@hiof.no</td>
+            </tr>
+            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                06.10.2023
+              </th>
+              <td className="px-6 py-4">Bedriften ble opprettet</td>
+              <td className="px-6 py-4">mariusca@hiof.no</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
