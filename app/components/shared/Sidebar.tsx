1e225806e16f085d3b3e95684a8ffd19a539c029
Marius Christian Aastebøl <mariusca@hiof.no>
2023-10-11 14:35:34 +0200

    Fixed link to bedrifter page

--- app/components/shared/Sidebar.tsx
+++ app/components/shared/Sidebar.tsx
@@ -118,7 +118,7 @@ const Sidebar = () => {
             </li>
             <li>
               <a
-                href="#"
+                href="/bedrifter"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
                 <svg
@@ -139,7 +139,7 @@ const Sidebar = () => {
             </li>
             <li>
               <a
-                href="#"
+                href="/#"
                 className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
               >
                 <svg
