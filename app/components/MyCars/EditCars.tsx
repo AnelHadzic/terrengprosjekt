be8713273bdca2155c989ff3edca72a5530eb850
Anel Hadzic <77800103+AnelHadzic@users.noreply.github.com>
2023-11-10 19:17:26 +0100

    Edited some logic

--- app/components/MyCars/EditCars.tsx
+++ app/components/MyCars/EditCars.tsx
@@ -5,7 +5,6 @@ import { toast } from "react-toastify"
 
 const EditCars = () => {
   const { userData, getUserData } = useUserDataContext()
-
   const [regNumbers, setRegNumbers] = useState<string[]>([""])
   const [primaryCar, setPrimaryCar] = useState<string>("")
 
@@ -37,6 +36,14 @@ const EditCars = () => {
     setRegNumbers([...regNumbers, ""])
   }
 
+  const setPrimaryCarAtIndex = (index: number) => {
+    if (regNumbers[index] !== primaryCar) {
+      setPrimaryCar(regNumbers[index])
+    } else {
+      setPrimaryCar("")
+    }
+  }
+
   const addNewRegList = async () => {
     if (primaryCar === "") {
       toast.error("Kunne ikke lagre endringer. Mangler primærbil.")
@@ -57,7 +64,6 @@ const EditCars = () => {
       })
         .then((response) => response.json())
         .then((data) => {
-          console.log("SUCCESS")
           getUserData()
           toast.success("Lagret endringer")
         })
@@ -92,9 +98,9 @@ const EditCars = () => {
 
               <button
                 type="button"
-                onClick={() => setPrimaryCar(regnr)}
+                onClick={() => setPrimaryCarAtIndex(index)}
                 className={
-                  regnr === primaryCar
+                  regnr !== "" && regnr === primaryCar
                     ? "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                     : "text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700"
                 }
