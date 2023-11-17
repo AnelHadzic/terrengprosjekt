6e7265b43c84962225674d90cb220f18f3a99bd2
Marius A <mariusca@hiof.no>
2023-11-17 12:06:59 +0100

    Updated user pages to use api/v2/users

--- app/components/User/UserList.tsx
+++ app/components/User/UserList.tsx
@@ -1,58 +1,35 @@
-import { ICompany } from "@/app/lib/interface/ICompany"
+import UserWithCompanyInfo from "@/app/lib/model/user/types/UserWithCompanyInfo"
+import { Result } from "@/app/types"
 import debounce from "@/app/utilities/debounce"
-import axios from "axios"
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime"
 import { useRouter } from "next/navigation"
-import { Fragment, useEffect, useRef, useState } from "react"
-
-type UserWithCompany = {
-  email: string
-  firstname?: string
-  lastname?: string
-  phone?: string
-  created?: Date
-  token?: string
-  role?: number
-  companyInfo?: ICompany
-  carRegNumbers?: string[]
-  primaryCarRegNumber?: string
-}
-
+import { Fragment, SetStateAction, useEffect, useRef, useState } from "react"
+import { toast } from "react-toastify"
 
 export default function UserList() {
-  const [userList, setUserList] = useState<UserWithCompany[]>([])
+  const [userList, setUserList] = useState<UserWithCompanyInfo[]>([])
   const router = useRouter()
   const [searchQuery, setSearchQuery] = useState("")
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState("")
 
   useEffect(() => {
-    setLoading(true)
-    setError("")
-    const fetchData = async () => {
-      try {
-        const API_URL = "/api/users"
-        const response = await axios.get(API_URL)
-        setUserList(response.data.data)
-      } catch (err) {
-        console.log(err)
-      } finally {
-        setLoading(false)
-      }
-    }
-
-    fetchData()
+    loadInitialUsers(setLoading, setError, setUserList)
   }, [])
 
   const debouncedFetchDataRef = useRef(
     debounce(async (query: string) => {
       try {
-        const API_URL = `/api/users?searchQuery=${query}`
-        const response = await axios.get(API_URL)
-        console.log(response.data.data)
-
-        const responseUsers = response.data.data as UserWithCompany[]
-
-        setUserList(responseUsers)
+        const API_URL = `/api/v2/users?searchQuery=${query}`
+        const response = await fetch(API_URL, { method: "GET" })
+        const result = (await response.json()) as Result<UserWithCompanyInfo[]>
+        if (result.success) {
+          setUserList(result.data)
+        } else {
+          toast.error(
+            `Støtte på en feil ved innhenting av brukere. Error: ${result.error}`,
+          )
+        }
       } catch (err) {
         console.log(err)
       } finally {
@@ -67,26 +44,9 @@ export default function UserList() {
     debouncedFetchDataRef.current(searchQuery)
   }, [searchQuery])
 
-  // const getCompanyName = (companyId: string): Promise<string> => {
-  //   let API_URL = `/api/company/${companyId}`
-  //   let response = await axios.get(API_URL)
-  //   const company = response.data.data as ICompany
-  //   return company.companyName
-  // }
-
   return (
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
-      <div className="p-1">
-        <input
-          type="text"
-          id="searchQuery"
-          name="searchQuery"
-          placeholder="Søk etter for eksempel navn, e-post, bilregnr eller lignende"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          className="border rounded-md p-2 w-full focus:outline-none focus:ring focus:border-blue-300"
-        />
-      </div>
+      {searchBar(searchQuery, setSearchQuery)}
 
       {loading && (
         <p className="p-6 bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-blue-100 dark:hover:bg-gray-900">
@@ -99,52 +59,106 @@ export default function UserList() {
         </p>
       )}
 
-      <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-          <tr>
-            <th scope="col" className="px-6 py-3">
-              E-post
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Bedrift
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Mobilnummer
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Bil regnr.
-            </th>
-            <th scope="col" className="px-6 py-3">
-              Aktiv parkering
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {userList.map((item, index) => (
-            <Fragment key={index}>
-              <tr
-                onClick={() => router.push(`/brukere/${item.email}`)}
-                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-blue-100 dark:hover:bg-gray-900 transition-all cursor-pointer"
-              >
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  {item.email}
-                </th>
-                <td className="px-6 py-4">
-                  {item.companyInfo === undefined
-                    ? "Ukjent Bedrift"
-                    : item.companyInfo?.companyName}
-                </td>
-                <td className="px-6 py-4">{item.phone}</td>
-                <td className="px-6 py-4">{item.primaryCarRegNumber}</td>
-                <td className="px-6 py-4">Temp aktiv status</td>
-              </tr>
-            </Fragment>
-          ))}
-        </tbody>
-      </table>
+      {userTable(userList, router)}
     </div>
   )
+}
+
+function searchBar(searchQuery: string, setSearchQuery: { (value: SetStateAction<string>): void; (arg0: string): void }) {
+  return <div className="p-1">
+    <input
+      type="text"
+      id="searchQuery"
+      name="searchQuery"
+      placeholder="Søk etter for eksempel navn, e-post, bilregnr eller lignende"
+      value={searchQuery}
+      onChange={(e) => setSearchQuery(e.target.value)}
+      className="border rounded-md p-2 w-full focus:outline-none focus:ring focus:border-blue-300" />
+  </div>
+}
+
+function userTable(
+  userList: UserWithCompanyInfo[],
+  router: AppRouterInstance | string[],
+) {
+  return (
+    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+      <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+        <tr>
+          <th scope="col" className="px-6 py-3">
+            E-post
+          </th>
+          <th scope="col" className="px-6 py-3">
+            Bedrift
+          </th>
+          <th scope="col" className="px-6 py-3">
+            Mobilnummer
+          </th>
+          <th scope="col" className="px-6 py-3">
+            Bil regnr.
+          </th>
+          <th scope="col" className="px-6 py-3">
+            Aktiv parkering
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        {userList.map((item, index) => (
+          <Fragment key={index}>
+            <tr
+              onClick={() => router.push(`/brukere/${item.email}`)}
+              className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-blue-100 dark:hover:bg-gray-900 transition-all cursor-pointer"
+            >
+              <th
+                scope="row"
+                className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+              >
+                {item.email}
+              </th>
+              <td className="px-6 py-4">
+                {item.companyInfo === undefined
+                  ? "Ukjent Bedrift"
+                  : item.companyInfo?.companyName}
+              </td>
+              <td className="px-6 py-4">{item.phone}</td>
+              <td className="px-6 py-4">{item.primaryCarRegNumber}</td>
+              <td className="px-6 py-4">Temp aktiv status</td>
+            </tr>
+          </Fragment>
+        ))}
+      </tbody>
+    </table>
+  )
+}
+
+function loadInitialUsers(
+  setLoading: { (value: SetStateAction<boolean>): void; (arg0: boolean): void },
+  setError: { (value: SetStateAction<string>): void; (arg0: string): void },
+  setUserList: {
+    (value: SetStateAction<UserWithCompanyInfo[]>): void
+    (arg0: UserWithCompanyInfo[]): void
+  },
+) {
+  setLoading(true)
+  setError("")
+  const fetchData = async () => {
+    try {
+      const API_URL = "/api/v2/users"
+      const response = await fetch(API_URL, { method: "GET" })
+      const result = (await response.json()) as Result<UserWithCompanyInfo[]>
+      if (result.success) {
+        setUserList(result.data)
+      } else {
+        toast.error(
+          `Støtte på en feil ved innhenting av brukere. Error: ${result.error}`,
+        )
+      }
+    } catch (err) {
+      console.log(err)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  fetchData()
 }
--- app/hooks/useUser.tsx
+++ app/hooks/useUser.tsx
@@ -3,10 +3,12 @@ import { useCallback, useState } from "react"
 import { useRouter } from "next/navigation"
 import { IUser } from "../lib/interface/IUser"
 import UserWithPopulatedCompany from "../lib/model/user/types/UserWithPopulatedCompany"
+import { Result } from "../types"
+import { toast } from "react-toastify"
 
 export function useUser(email: string) {
   const router = useRouter()
-  const decodedEditedEmail = decodeURIComponent(email);
+  const decodedEditedEmail = decodeURIComponent(email)
 
   const [user, setUser] = useState<UserWithPopulatedCompany | undefined>()
   const [isEditing, setIsEditing] = useState(false)
@@ -23,10 +25,16 @@ export function useUser(email: string) {
 
   const fetchUserData = useCallback(async () => {
     try {
-      const API_URL = `/api/users/${email}`
-      const response = await axios.get(API_URL)
-      setUser(response.data.data)
+      const API_URL = `/api/v2/users/${email}`
+      const response = await fetch(API_URL, { method: "GET" })
+      const result = (await response.json()) as Result<UserWithPopulatedCompany>
+      if (result.success) {
+        setUser(result.data)
+      } else {
+        toast.error(`Kunne ikke hente inn brukeren. Error: ${result.error}`)
+      }
     } catch (err) {
+      toast.error(`Feil i kommunikasjon med server. Error: ${err}`)
       console.log(err)
     }
   }, [email])
@@ -62,26 +70,44 @@ export function useUser(email: string) {
   }
 
   const handleDeleteClick = async () => {
-    console.log("test")
     try {
-      const API_URL = `/api/users/${email}`
-      await axios.delete(API_URL)
-      router.push("/bedrifter")
+      const API_URL = `/api/v2/users/${email}`
+      const response = await fetch(API_URL, { method: "DELETE" })
+      const result = (await response.json()) as Result<IUser>
+      if (result.success) {
+        toast.success(`Slettet brukeren med e-post "${result.data.email}"`)
+        router.push("/brukere")
+      } else {
+        toast.error(`Kunne ikke slette brukeren. Error: ${result.error}`)
+      }
     } catch (err) {
+      toast.error(`Feil i kommunikasjon med server. Error: ${err}`)
       console.log(err)
     }
   }
 
   const handleSaveClick = async () => {
     try {
-      const API_URL = `/api/users/${email}`
-      await axios.patch(API_URL, editedUser)
-      setIsEditing(false)
+      const API_URL = `/api/v2/users/${email}`
+      const response = await fetch(API_URL, {
+        method: "PATCH",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(editedUser),
+      })
+      const result = (await response.json()) as Result<IUser>
+      if (result.success) {
+        toast.success("Endringene ble lagret.")
+        setIsEditing(false)
 
-      if (decodedEditedEmail === editedUser.email) {
-        fetchUserData()
+        if (decodedEditedEmail === editedUser.email) {
+          fetchUserData()
+        } else {
+          router.push(`/brukere/${editedUser.email}`)
+        }
       } else {
-        router.push(`/brukere/${editedUser.email}`)
+        toast.error(`Kunne ikke lagre dine endringer. ${result.error}`)
       }
     } catch (err) {
       console.log(err)
