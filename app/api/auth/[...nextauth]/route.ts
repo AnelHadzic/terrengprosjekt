e8328a50c98c46aff6f406df9efff95e76ae5f62
Anel Hadzic <77800103+AnelHadzic@users.noreply.github.com>
2023-12-06 10:41:20 +0100

    revert

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,6 @@
 import NextAuth from "next-auth/next"
 import { options } from "./options"
-import { NextApiRequest, NextApiResponse } from "next"
 
-export default async function auth(req: NextApiRequest, res: NextApiResponse) {
-  const userAgent = req.headers["user-agent"]
+const handler = NextAuth(options)
 
-  if (
-    req.method === "HEAD" ||
-    (userAgent && /lua-resty-http.+ngx_lua/.test(userAgent))
-  ) {
-    console.log("Bot detected")
-    return res.status(200).end()
-  }
-  return await NextAuth(req, res, options)
-}
+export { handler as GET, handler as POST }
